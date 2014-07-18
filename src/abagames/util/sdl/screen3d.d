acbb095fea3ea881465dce20cc93432b4c444ae4
Ben Boeckel <mathstuf@gmail.com>
2014-07-18 00:49:00 -0400

    screen: remove unused methods

--- src/abagames/util/sdl/screen3d.d
+++ src/abagames/util/sdl/screen3d.d
@@ -152,22 +152,6 @@ public class Screen3D: Screen, SizableScreen {
     return _height;
   }
 
-  public static void glVertex(vec2 v) {
-    glVertex3f(v.x, v.y, 0);
-  }
-
-  public static void glVertex(vec3 v) {
-    glVertex3f(v.x, v.y, v.z);
-  }
-
-  public static void glTranslate(vec2 v) {
-    glTranslatef(v.x, v.y, 0);
-  }
-
-  public static void glTranslate(vec3 v) {
-    glTranslatef(v.x, v.y, v.z);
-  }
-
   public static void setColor(float r, float g, float b, float a = 1) {
     glColor4f(r * _brightness, g * _brightness, b * _brightness, a);
   }
